a81640ba6589ecf0972931211fab1b1aa344921f
Abhishek Roy <abhishekroy@Abhisheks-MacBook-Pro.local>
2025-06-21 01:03:40 +0530

    initial commit

---
+++ src/test/java/com/roy/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
